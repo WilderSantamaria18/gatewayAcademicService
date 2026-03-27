a554a1517d3c8fb93475ad6259ef9ef9ba0befd3
wilder <ln60763412@idat.pe>
2026-03-26 20:00:05 -0500

    Initial commit

---
+++ src/main/java/pe/edu/idat/app_gateway/AppGatewayApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppGatewayApplication.class, args);
+	}
+
+}
